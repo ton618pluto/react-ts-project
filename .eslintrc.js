478d73365ee04c896f0d6dc0da98c41e16e4dc9a
hhh <3382612206@qq.com>
2026-01-11 13:46:21 +0800

    refactor: 新建项目、lint、prettier和husky配置

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:eslint-plugin-prettier/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "settings":{
+        react:{
+            version:"detect",
+        }
+    },
+    "rules": {
+    }
+}
