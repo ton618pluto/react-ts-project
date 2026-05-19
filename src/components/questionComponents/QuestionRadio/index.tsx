9608588670d0bc09b3e8dd1cb914d404f8f8e332
hhh <3382612206@qq.com>
2026-05-20 01:53:39 +0800

    fix: normalize line endings to LF

--- src/components/questionComponents/QuestionRadio/index.tsx
+++ src/components/questionComponents/QuestionRadio/index.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
+
 import { Typography, Radio, Space } from 'antd'
+
 import { QuestionRadioPropsType } from '@/types/questionTypes'
+
 import { QuestionRadioDefaultProps } from './default'
 
 const { Paragraph } = Typography
@@ -8,15 +11,20 @@ const { Paragraph } = Typography
 const QutstionRadio: FC<QuestionRadioPropsType> = (props: QuestionRadioPropsType) => {
   const {
     title,
+
     isVertical,
+
     value,
+
     options = [],
+
     onChange,
   } = { ...QuestionRadioDefaultProps, ...props }
 
   return (
     <div>
       <Paragraph strong>{title}</Paragraph>
+
       <Radio.Group
         value={value}
         onChange={e => {
@@ -28,6 +36,7 @@ const QutstionRadio: FC<QuestionRadioPropsType> = (props: QuestionRadioPropsType
         <Space orientation={isVertical ? 'vertical' : 'horizontal'}>
           {options.map(opt => {
             const { value, text } = opt
+
             return (
               <Radio value={value} key={value}>
                 {text}
